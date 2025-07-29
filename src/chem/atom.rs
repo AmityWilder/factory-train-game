9890c22bba9a48453b0894e2ed9e5f2f1711a9e5
Amy Wilder <henrythepony@gmail.com>
2025-07-29 10:43:20 -0400

    Add atom builder and work on neutron stability

--- src/chem/atom.rs
+++ src/chem/atom.rs
@@ -2,10 +2,323 @@ use super::element::Element;
 use super::units::{ELECTRON_MASS, NEUTRON_MASS, PROTON_MASS};
 use crate::chem::fmt::Superscript;
 
+macro_rules! isotopes {
+    ($($element:ident $neutrons:literal),* $(,)?) => {
+        [$(Atom { element: Element::$element, neutrons: $neutrons, electrons: const { Element::$element.protons().get() } }),*]
+    };
+}
+
+pub static PRIMORDIAL_ISOTOPES: [Atom; 304] = isotopes![
+    Sn 120,
+    Sn 118,
+    Sn 116,
+    Sn 119,
+    Sn 117,
+    Sn 124,
+    Sn 122,
+    Sn 112,
+    Sn 114,
+    Sn 115,
+    Xe 132,
+    Xe 129,
+    Xe 131,
+    Xe 134,
+    Xe 136, // unstable
+    Xe 130,
+    Xe 128,
+    Xe 124, // unstable
+    Xe 126,
+    Cd 114,
+    Cd 112,
+    Cd 111,
+    Cd 110,
+    Cd 113, // unstable
+    Cd 116, // unstable
+    Cd 106,
+    Cd 108,
+    Te 130, // unstable
+    Te 128, // unstable
+    Te 126,
+    Te 125,
+    Te 124,
+    Te 122,
+    Te 123,
+    Te 120,
+    Ru 102,
+    Ru 104,
+    Ru 101,
+    Ru 99,
+    Ru 100,
+    Ru 96,
+    Ru 98,
+    Dy 164,
+    Dy 162,
+    Dy 163,
+    Dy 161,
+    Dy 160,
+    Dy 158,
+    Dy 156,
+    Yb 174,
+    Yb 172,
+    Yb 173,
+    Yb 171,
+    Yb 176,
+    Yb 170,
+    Yb 168,
+    Hg 202,
+    Hg 200,
+    Hg 199,
+    Hg 201,
+    Hg 198,
+    Hg 204,
+    Hg 196,
+    Mo 98,
+    Mo 96,
+    Mo 95,
+    Mo 92,
+    Mo 100, // unstable
+    Mo 97,
+    Mo 94,
+    Ba 138,
+    Ba 137,
+    Ba 136,
+    Ba 135,
+    Ba 134,
+    Ba 132,
+    Ba 130, // unstable
+    Gd 158,
+    Gd 160,
+    Gd 156,
+    Gd 157,
+    Gd 155,
+    Gd 154,
+    Gd 152, // unstable
+    Nd 142,
+    Nd 144, // unstable
+    Nd 146,
+    Nd 143,
+    Nd 145,
+    Nd 148,
+    Nd 150, // unstable
+    Sm 152,
+    Sm 154,
+    Sm 147, // unstable
+    Sm 149,
+    Sm 148, // unstable
+    Sm 150,
+    Sm 144,
+    Os 192,
+    Os 190,
+    Os 189,
+    Os 188,
+    Os 187,
+    Os 186, // unstable
+    Os 184, // unstable
+    Pd 106,
+    Pd 108,
+    Pd 105,
+    Pd 110,
+    Pd 104,
+    Pd 102,
+    Er 166,
+    Er 168,
+    Er 167,
+    Er 170,
+    Er 164,
+    Er 162,
+    Ca 40,
+    Ca 44,
+    Ca 42,
+    Ca 43,
+    Ca 46,
+    Se 80,
+    Se 78,
+    Se 76,
+    Se 82, // unstable
+    Se 77,
+    Se 74,
+    Kr 84,
+    Kr 86,
+    Kr 82,
+    Kr 83,
+    Kr 80,
+    Kr 78, // unstable
+    Hf 180,
+    Hf 178,
+    Hf 177,
+    Hf 179,
+    Hf 176,
+    Hf 174, // unstable
+    Pt 195,
+    Pt 194,
+    Pt 196,
+    Pt 198,
+    Pt 192,
+    Pt 190, // unstable
+    Ti 48,
+    Ti 46,
+    Ti 47,
+    Ti 49,
+    Ti 50,
+    Ni 58,
+    Ni 60,
+    Ni 61,
+    Ni 64,
+    Zn 64,
+    Zn 66,
+    Zn 68,
+    Zn 67,
+    Zn 70,
+    Ge 74,
+    Ge 72,
+    Ge 70,
+    Ge 73,
+    Ge 76, // unstable
+    Zr 90,
+    Zr 94,
+    Zr 92,
+    Zr 91,
+    Zr 96, // unstable
+    W 184,
+    W 186,
+    W 182,
+    W 183,
+    W 180, // unstable
+    S 32,
+    S 34,
+    S 33,
+    S 36,
+    Cr 52,
+    Cr 53,
+    Cr 50,
+    Cr 54,
+    Fe 54,
+    Fe 57,
+    Fe 58,
+    Sr 88,
+    Sr 86,
+    Sr 87,
+    Sr 84,
+    Ce 140,
+    Ce 142,
+    Ce 138,
+    Ce 136,
+    Pb 208,
+    Pb 206,
+    Pb 207,
+    Pb 204,
+    O 16,
+    O 17,
+    Ne 20,
+    Ne 22,
+    Ne 21,
+    Mg 24,
+    Mg 26,
+    Mg 25,
+    Si 28,
+    Si 29,
+    Si 30,
+    Ar 40,
+    Ar 36,
+    Ar 38,
+    Be 9,
+    K 39,
+    K 41,
+    Li 7,
+    Li 6,
+    B 11,
+    B 10,
+    N 14,
+    N 15,
+    Cl 35,
+    Cu 63,
+    Cu 65,
+    Ga 69,
+    Ga 71,
+    Br 79,
+    Br 81,
+    Ag 107,
+    Ag 109,
+    Sb 121,
+    Sb 123,
+    Ta 181,
+    Ir 193,
+    Ir 191,
+    Tl 205,
+    Tl 203,
+    V 51,
+    V 50, // unstable
+    Rb 85,
+    Rb 87, // unstable
+    In 115, // unstable
+    In 113,
+    La 139,
+    La 138, // unstable
+    Eu 153,
+    Eu 151, // unstable
+    Lu 175,
+    Lu 176, // unstable
+    Re 187, // unstable
+    Re 185,
+    F 19,
+    Na 23,
+    Al 27,
+    P 31,
+    Sc 45,
+    Mn 55,
+    Co 59,
+    As 75,
+    Y 89,
+    Nb 93,
+    Rh 103,
+    I 127,
+    Cs 133,
+    Pr 141,
+    Tb 159,
+    Ho 165,
+    Tm 169,
+    Au 197,
+    Pu 244,
+    Cm 247,
+    Tc 97,
+    Np 237,
+    Pa 231,
+    Am 243,
+    Ra 226,
+    Bk 247,
+    Cf 251,
+    Po 209,
+    Ac 227,
+    Pm 145,
+    Es 252,
+    Fm 257,
+    Md 258,
+    Rn 222,
+    Db 268,
+    Lr 266,
+    At 210,
+    No 259,
+    Rf 267,
+    Fr 223,
+    Sg 269,
+    Bh 270,
+    Rg 282,
+    Cn 285,
+    Hs 269,
+    Ds 281,
+    Nh 286,
+    Mt 278,
+    Fl 289,
+    Mc 290,
+    Lv 293,
+    Ts 294,
+    Og 294,
+];
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Atom {
     pub element: Element,
-    pub neutrons: u8,
+    pub neutrons: u16,
     pub electrons: u8,
 }
 
@@ -19,15 +332,74 @@ impl Default for Atom {
     }
 }
 
-impl Atom {
-    pub const fn neutral(element: Element) -> Self {
+#[derive(Debug, Clone)]
+pub struct AtomBuilder {
+    atom: Atom,
+}
+
+impl AtomBuilder {
+    pub const fn new(element: Element) -> Self {
         Self {
-            element,
-            neutrons: element.protons().get(),
-            electrons: element.protons().get(),
+            atom: Atom {
+                element,
+                neutrons: 0,
+                electrons: 0,
+            },
         }
     }
 
+    pub const fn neutral(&mut self) -> &mut Self {
+        self.atom.electrons = self.atom.element.protons().get();
+        self
+    }
+
+    pub const fn charge(&mut self, charge: i8) -> Result<&mut Self, &mut Self> {
+        const U8_MIN: i16 = u8::MIN as i16;
+        const U8_MAX: i16 = u8::MAX as i16;
+        match self.atom.element.protons().get() as i16 - charge as i16 {
+            electrons @ U8_MIN..=U8_MAX =>
+            #[allow(
+                clippy::cast_possible_truncation,
+                clippy::cast_sign_loss,
+                reason = "cannot in this branch"
+            )]
+            {
+                self.atom.electrons = electrons as u8;
+                Ok(self)
+            }
+            _ => Err(self),
+        }
+    }
+
+    pub const fn electrons(&mut self, electrons: u8) -> &mut Self {
+        self.atom.electrons = electrons;
+        self
+    }
+
+    pub const fn stable(&mut self) -> &mut Self {
+        // TODO: This is not necessarily stable...
+        self.atom.neutrons = self.atom.element.protons().get() as u16;
+        self
+    }
+
+    pub const fn neutrons(&mut self, neutrons: u16) -> &mut Self {
+        self.atom.neutrons = neutrons;
+        self
+    }
+
+    pub const fn build(&mut self) -> Atom {
+        self.atom
+    }
+}
+
+impl Element {
+    #[inline]
+    pub const fn atom(self) -> AtomBuilder {
+        AtomBuilder::new(self)
+    }
+}
+
+impl Atom {
     /// The name of the isotope
     ///
     /// Returns [`None`] if the isotope has no meaningful name.
@@ -56,6 +428,9 @@ impl Atom {
             + self.electrons as f64 * ELECTRON_MASS
     }
 
+    /// Charge of the atom
+    /// - negative = more electrons
+    /// - positive = fewer electrons
     pub const fn charge(self) -> i16 {
         self.element.protons().get() as i16 - self.electrons as i16
     }
@@ -63,17 +438,17 @@ impl Atom {
 
 impl std::fmt::Display for Atom {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.element)?;
+        let element = self.element;
         let charge = self.charge();
-        let mag = charge.unsigned_abs();
-        if mag > 1 {
-            Superscript::fmt(&mag, f)?;
+        let (sign, mag) = (
+            Superscript(char::from(b"+-"[usize::from(charge.is_negative())])),
+            Superscript(charge.unsigned_abs()),
+        );
+        match mag.0 {
+            0 => write!(f, "{element}"),
+            1 => write!(f, "{element}{sign}"),
+            2.. => write!(f, "{element}{mag}{sign}"),
         }
-        if mag > 0 {
-            let sign = if charge.is_negative() { '-' } else { '+' };
-            Superscript::fmt(&sign, f)?;
-        }
-        Ok(())
     }
 }
 
@@ -83,50 +458,12 @@ mod tests {
 
     #[test]
     fn test0() {
-        assert_eq!(
-            &Atom {
-                element: Element::He,
-                neutrons: 2,
-                electrons: 2,
-            }
-            .to_string(),
-            "He"
-        );
-        assert_eq!(
-            &Atom {
-                element: Element::He,
-                neutrons: 2,
-                electrons: 3,
-            }
-            .to_string(),
-            "He⁻"
-        );
-        assert_eq!(
-            &Atom {
-                element: Element::He,
-                neutrons: 2,
-                electrons: 4,
-            }
-            .to_string(),
-            "He²⁻"
-        );
-        assert_eq!(
-            &Atom {
-                element: Element::He,
-                neutrons: 2,
-                electrons: 1,
-            }
-            .to_string(),
-            "He⁺"
-        );
-        assert_eq!(
-            &Atom {
-                element: Element::He,
-                neutrons: 2,
-                electrons: 0,
-            }
-            .to_string(),
-            "He²⁺"
-        );
+        use Element::He;
+
+        assert_eq!(He.atom().charge(0).unwrap().build().to_string(), "He");
+        assert_eq!(He.atom().charge(-1).unwrap().build().to_string(), "He⁻");
+        assert_eq!(He.atom().charge(-2).unwrap().build().to_string(), "He²⁻");
+        assert_eq!(He.atom().charge(1).unwrap().build().to_string(), "He⁺");
+        assert_eq!(He.atom().charge(2).unwrap().build().to_string(), "He²⁺");
     }
 }
--- src/chem/fmt.rs
+++ src/chem/fmt.rs
@@ -1,5 +1,147 @@
 use arrayvec::ArrayString;
-use std::fmt::Write;
+
+pub enum MathSymbol {
+    UpperAlpha,
+    LowerAlpha,
+    UpperBeta,
+    LowerBeta,
+    UpperGamma,
+    LowerGamma,
+    UpperDelta,
+    LowerDelta,
+    UpperEpsilon,
+    LowerEpsilon,
+    UpperZeta,
+    LowerZeta,
+    UpperEta,
+    LowerEta,
+    UpperTheta,
+    LowerTheta,
+    UpperIota,
+    LowerIota,
+    UpperKappa,
+    LowerKappa,
+    UpperLambda,
+    LowerLambda,
+    UpperMu,
+    LowerMu,
+    UpperNu,
+    LowerNu,
+    UpperXi,
+    LowerXi,
+    UpperOmicron,
+    LowerOmicron,
+    UpperPi,
+    LowerPi,
+    UpperRho,
+    LowerRho,
+    UpperSigma,
+    LowerSigma,
+    FinalSigma,
+    UpperTau,
+    LowerTau,
+    UpperUpsilon,
+    LowerUpsilon,
+    UpperPhi,
+    LowerPhi,
+    UpperChi,
+    LowerChi,
+    UpperPsi,
+    LowerPsi,
+    UpperOmega,
+    LowerOmega,
+}
+
+impl std::fmt::Display for MathSymbol {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        std::fmt::Display::fmt(
+            &match self {
+                Self::UpperAlpha => 'Α',
+                Self::LowerAlpha => 'α',
+                Self::UpperBeta => 'Β',
+                Self::LowerBeta => 'β',
+                Self::UpperGamma => 'Γ',
+                Self::LowerGamma => 'γ',
+                Self::UpperDelta => 'Δ',
+                Self::LowerDelta => 'δ',
+                Self::UpperEpsilon => 'Ε',
+                Self::LowerEpsilon => 'ε',
+                Self::UpperZeta => 'Ζ',
+                Self::LowerZeta => 'ζ',
+                Self::UpperEta => 'Η',
+                Self::LowerEta => 'η',
+                Self::UpperTheta => 'Θ',
+                Self::LowerTheta => 'θ',
+                Self::UpperIota => 'Ι',
+                Self::LowerIota => 'ι',
+                Self::UpperKappa => 'Κ',
+                Self::LowerKappa => 'κ',
+                Self::UpperLambda => 'Λ',
+                Self::LowerLambda => 'λ',
+                Self::UpperMu => 'Μ',
+                Self::LowerMu => 'μ',
+                Self::UpperNu => 'Ν',
+                Self::LowerNu => 'ν',
+                Self::UpperXi => 'Ξ',
+                Self::LowerXi => 'ξ',
+                Self::UpperOmicron => 'Ο',
+                Self::LowerOmicron => 'ο',
+                Self::UpperPi => 'Π',
+                Self::LowerPi => 'π',
+                Self::UpperRho => 'Ρ',
+                Self::LowerRho => 'ρ',
+                Self::UpperSigma => 'Σ',
+                Self::LowerSigma => 'σ',
+                Self::FinalSigma => 'ς',
+                Self::UpperTau => 'Τ',
+                Self::LowerTau => 'τ',
+                Self::UpperUpsilon => 'Υ',
+                Self::LowerUpsilon => 'υ',
+                Self::UpperPhi => 'Φ',
+                Self::LowerPhi => 'φ',
+                Self::UpperChi => 'Χ',
+                Self::LowerChi => 'χ',
+                Self::UpperPsi => 'Ψ',
+                Self::LowerPsi => 'ψ',
+                Self::UpperOmega => 'Ω',
+                Self::LowerOmega => 'ω',
+            },
+            f,
+        )
+    }
+}
+
+pub const SUP_0: char = '⁰';
+pub const SUP_1: char = '¹';
+pub const SUP_2: char = '²';
+pub const SUP_3: char = '³';
+pub const SUP_4: char = '⁴';
+pub const SUP_5: char = '⁵';
+pub const SUP_6: char = '⁶';
+pub const SUP_7: char = '⁷';
+pub const SUP_8: char = '⁸';
+pub const SUP_9: char = '⁹';
+pub const SUP_PLUS: char = '⁺';
+pub const SUP_HYPHEN: char = '⁻';
+pub const SUP_EQUAL: char = '⁼';
+pub const SUP_LPAREN: char = '⁽';
+pub const SUP_RPAREN: char = '⁾';
+
+pub const SUB_0: char = '₀';
+pub const SUB_1: char = '₁';
+pub const SUB_2: char = '₂';
+pub const SUB_3: char = '₃';
+pub const SUB_4: char = '₄';
+pub const SUB_5: char = '₅';
+pub const SUB_6: char = '₆';
+pub const SUB_7: char = '₇';
+pub const SUB_8: char = '₈';
+pub const SUB_9: char = '₉';
+pub const SUB_PLUS: char = '₊';
+pub const SUB_HYPHEN: char = '₋';
+pub const SUB_EQUAL: char = '₌';
+pub const SUB_LPAREN: char = '₍';
+pub const SUB_RPAREN: char = '₎';
 
 #[const_trait]
 pub trait SubSupScript: Sized {
@@ -25,21 +167,21 @@ impl const SubSupScript for char {
     #[inline]
     fn to_superscript(self) -> Option<Self::Output> {
         match self {
-            '0' => Some('⁰'),
-            '1' => Some('¹'),
-            '2' => Some('²'),
-            '3' => Some('³'),
-            '4' => Some('⁴'),
-            '5' => Some('⁵'),
-            '6' => Some('⁶'),
-            '7' => Some('⁷'),
-            '8' => Some('⁸'),
-            '9' => Some('⁹'),
-            '+' => Some('⁺'),
-            '-' => Some('⁻'),
-            '=' => Some('⁼'),
-            '(' => Some('⁽'),
-            ')' => Some('⁾'),
+            '0' => Some(SUP_0),
+            '1' => Some(SUP_1),
+            '2' => Some(SUP_2),
+            '3' => Some(SUP_3),
+            '4' => Some(SUP_4),
+            '5' => Some(SUP_5),
+            '6' => Some(SUP_6),
+            '7' => Some(SUP_7),
+            '8' => Some(SUP_8),
+            '9' => Some(SUP_9),
+            '+' => Some(SUP_PLUS),
+            '-' => Some(SUP_HYPHEN),
+            '=' => Some(SUP_EQUAL),
+            '(' => Some(SUP_LPAREN),
+            ')' => Some(SUP_RPAREN),
             _ => None,
         }
     }
@@ -47,87 +189,138 @@ impl const SubSupScript for char {
     #[inline]
     fn to_subscript(self) -> Option<Self::Output> {
         match self {
-            '0' => Some('₀'),
-            '1' => Some('₁'),
-            '2' => Some('₂'),
-            '3' => Some('₃'),
-            '4' => Some('₄'),
-            '5' => Some('₅'),
-            '6' => Some('₆'),
-            '7' => Some('₇'),
-            '8' => Some('₈'),
-            '9' => Some('₉'),
-            '+' => Some('₊'),
-            '-' => Some('₋'),
-            '=' => Some('₌'),
-            '(' => Some('₍'),
-            ')' => Some('₎'),
+            '0' => Some(SUB_0),
+            '1' => Some(SUB_1),
+            '2' => Some(SUB_2),
+            '3' => Some(SUB_3),
+            '4' => Some(SUB_4),
+            '5' => Some(SUB_5),
+            '6' => Some(SUB_6),
+            '7' => Some(SUB_7),
+            '8' => Some(SUB_8),
+            '9' => Some(SUB_9),
+            '+' => Some(SUB_PLUS),
+            '-' => Some(SUB_HYPHEN),
+            '=' => Some(SUB_EQUAL),
+            '(' => Some(SUB_LPAREN),
+            ')' => Some(SUB_RPAREN),
             _ => None,
         }
     }
 }
 
-pub trait Superscript {
+pub trait DisplaySuperscript {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result;
 }
 
-impl Superscript for char {
+impl DisplaySuperscript for char {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         self.to_superscript()
             .ok_or(std::fmt::Error)
-            .and_then(|ch| f.write_char(ch))
+            .and_then(|ch| std::fmt::Write::write_char(f, ch))
     }
 }
 
-impl Superscript for str {
+impl DisplaySuperscript for str {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         for ch in self.chars() {
-            Superscript::fmt(&ch, f)?;
+            DisplaySuperscript::fmt(&ch, f)?;
         }
         Ok(())
     }
 }
 
-pub trait Subscript {
+pub struct Superscript<T: ?Sized + DisplaySuperscript>(pub T);
+
+impl<T: ?Sized + DisplaySuperscript> std::fmt::Display for Superscript<T> {
+    #[inline]
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        DisplaySuperscript::fmt(&self.0, f)
+    }
+}
+
+impl<T: ?Sized + DisplaySuperscript> DisplaySuperscript for &T {
+    #[inline]
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        DisplaySuperscript::fmt(&**self, f)
+    }
+}
+
+pub trait DisplaySubscript {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result;
 }
 
-impl Subscript for char {
+impl DisplaySubscript for char {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         self.to_subscript()
             .ok_or(std::fmt::Error)
-            .and_then(|ch| f.write_char(ch))
+            .and_then(|ch| std::fmt::Write::write_char(f, ch))
     }
 }
 
-impl Subscript for str {
+impl DisplaySubscript for str {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         for ch in self.chars() {
-            Subscript::fmt(&ch, f)?;
+            DisplaySubscript::fmt(&ch, f)?;
         }
         Ok(())
+    }
+}
+
+impl<T: ?Sized + DisplaySubscript> DisplaySubscript for &T {
+    #[inline]
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        DisplaySubscript::fmt(&**self, f)
+    }
+}
+
+pub struct Subscript<T: ?Sized + DisplaySubscript>(pub T);
+
+impl<T: ?Sized + DisplaySubscript> std::fmt::Display for Subscript<T> {
+    #[inline]
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        DisplaySubscript::fmt(&self.0, f)
     }
 }
 
 macro_rules! impl_num_scripts {
     ($($T:ty),* $(,)?) => {$(
-        impl Superscript for $T {
+        impl DisplaySuperscript for $T {
             fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
                 let mut buf = ArrayString::<64>::new();
-                write!(buf, "{}", self)?;
-                Superscript::fmt(buf.as_str(), f)
+                std::fmt::Write::write_fmt(&mut buf, format_args!("{}", self))?;
+                DisplaySuperscript::fmt(buf.as_str(), f)
             }
         }
-        impl Subscript for $T {
+        impl DisplaySubscript for $T {
             fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
                 let mut buf = ArrayString::<64>::new();
-                write!(buf, "{}", self)?;
-                Subscript::fmt(buf.as_str(), f)
+                std::fmt::Write::write_fmt(&mut buf, format_args!("{}", self))?;
+                DisplaySubscript::fmt(buf.as_str(), f)
             }
         }
     )*};
 }
 
 impl_num_scripts! {
-    u8, i8, u16, i16, u32, i32, u64, i64, u128, i128, usize, isize, f32, f64
+    u8, i8, u16, i16, u32, i32, u64, i64, u128, i128, usize, isize,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_superscript() {
+        assert_eq!(&Superscript('+').to_string(), "⁺");
+        assert_eq!(&Superscript("2(140+73)=426").to_string(), "²⁽¹⁴⁰⁺⁷³⁾⁼⁴²⁶");
+        assert_eq!(&Superscript(-65i8).to_string(), "⁻⁶⁵");
+    }
+
+    #[test]
+    fn test_subscript() {
+        assert_eq!(&Subscript('+').to_string(), "₊");
+        assert_eq!(&Subscript("2(140+73)=426").to_string(), "₂₍₁₄₀₊₇₃₎₌₄₂₆");
+        assert_eq!(&Subscript(-65i8).to_string(), "₋₆₅");
+    }
 }
